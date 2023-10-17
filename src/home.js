f7cc6e57eb3bdb256ca4426a49fab85d9692d58d
Ohcatin <catalinajaviera.ordenes@gmail.com>
2023-10-17 19:08:06 -0300

    cambios de home

--- src/home.js
+++ src/home.js
@@ -2,16 +2,20 @@
 function home(navigateTo) {
     const sectionHome = document.createElement('section');
     sectionHome.setAttribute("class", "containerHome");
+    const img = document.createElement('img');
+    img.setAttribute("class", "logo");
     const welcome = document.createElement('h2');
     welcome.setAttribute("class", "welcome");
     const nameSocial = document.createElement('h1');
     nameSocial.setAttribute("class", "nameSocial");
+    const descriptionSocial = document.createElement('h3');
+    descriptionSocial.setAttribute("class", "descriptionSocial");
     const buttonOne = document.createElement('button');
-    buttonOne.setAttribute("class", "signIn")
+    buttonOne.setAttribute("class", "signIn");
     const buttonTwo = document.createElement('button');
-    buttonTwo.setAttribute("class", "signUp")
-    const img = document.createElement('img');
-    img.setAttribute("class", "logo")
+    buttonTwo.setAttribute("class", "signUp");    
+    const description2Social = document.createElement('h4');
+    description2Social.setAttribute("class", "description2Social");
 
     img.src = "imagen/LogoEnRutados.png";
     buttonOne.textContent = 'Iniciar sesión';
@@ -24,10 +28,12 @@ function home(navigateTo) {
       navigateTo('/login');
     });
   
-    welcome.textContent = 'Bienvenido';
+    welcome.textContent = 'Bienvenido a';
     nameSocial.textContent = 'EnRutados';
+    descriptionSocial.textContent = '¡Tu red social de viajes favoritas!';
+    description2Social.textContent = 'O conectate con';
   
-    sectionHome.append(welcome, buttonOne, buttonTwo, img, nameSocial);
+    sectionHome.append(img, welcome,nameSocial, descriptionSocial, buttonOne, buttonTwo, description2Social);
     console.log("soy el home");
     return sectionHome;
   }
