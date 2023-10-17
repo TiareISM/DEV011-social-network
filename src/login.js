21459c424dd81580789e8bb2f91e465d6ad195b6
Tiare Infante <tiare.infante.snmartin@gmail.com>
2023-10-17 19:02:59 -0300

    login y google

--- src/login.js
+++ src/login.js
@@ -1,28 +1,55 @@
 // file login finished
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
 function login(navigateTo) {
-    const section = document.createElement('section');
-    const title = document.createElement('h2');
-    const buttonReturn = document.createElement('button');
-    const form = document.createElement('form');
-    const inputEmail = document.createElement('input');
-    const inputPass = document.createElement('input');
-    const buttonLogin = document.createElement('button');
-  
-    inputEmail.placeholder = 'Write email';
-    inputPass.placeholder = 'pass';
-  
-    title.textContent = 'Login';
-    buttonLogin.textContent = 'go';
-  
-    buttonReturn.textContent = 'Return to home';
-    buttonReturn.addEventListener('click', () => {
-      navigateTo('/');
-    });
-  
-    form.append(inputEmail, inputPass, buttonLogin);
-    section.append(title, form, buttonReturn);
-  
-    return section;
-  }
-  
-  export default login;
+  const buttonReturn = document.createElement('button');
+  buttonReturn.setAttribute('class', 'return');
+  const sectionContainerName = document.createElement('section');
+  sectionContainerName.setAttribute('class', 'containerName');
+  const img = document.createElement('img');
+  img.setAttribute('class', 'logo');
+  const nameSocial = document.createElement('h1');
+  nameSocial.setAttribute('class', 'nameSocial');
+  const sectionContainerSingIn = document.createElement('section');
+  sectionContainerSingIn.setAttribute('class', 'containerSingIn');
+  const form = document.createElement('form');
+  const inputEmail = document.createElement('input');
+  const inputPass = document.createElement('input');
+  const buttonLogin = document.createElement('button');
+  const forgetPassword = document.createElement('input');
+  const openGoogle = document.createElement('button');
+  img.src = 'imagen/LogoEnRutados.png';
+  inputEmail.placeholder = 'Write email';
+  inputPass.placeholder = 'pass';
+
+  nameSocial.textContent = 'Login';
+  buttonLogin.textContent = 'go';
+  openGoogle.textContent = 'Conectate con Google';
+
+  buttonReturn.textContent = 'Return to home';
+  buttonReturn.addEventListener('click', () => {
+    navigateTo('/');
+  });
+
+  // Agrega un evento de click para el botón de inicio de sesión con Google
+  openGoogle.addEventListener('click', async () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    try {
+      await signInWithPopup(auth, provider);
+      // Inicio de sesión con Google exitoso
+      // Puedes agregar una redirección o lógica adicional aquí
+    } catch (error) {
+      console.error('Google login error', error);
+      // Maneja los errores de inicio de sesión con Google
+    }
+  });
+
+  form.append(inputEmail, inputPass, buttonLogin);
+  sectionContainerName.append(img, nameSocial, form, buttonReturn, openGoogle);
+
+  return sectionContainerName;
+}
+
+export default login;
--- src/main.js
+++ src/main.js
@@ -1,35 +1,38 @@
-import firebase from "firebase/compat/app";
+import firebase from 'firebase/compat/app';
 // Required for side-effects
-import "firebase/firestore";
+import 'firebase/firestore';
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-    apiKey: 'AIzaSyCjGsgPgUBt6lk3jxRAuYbyG4DGVxcdesY',
-    authDomain: 'enrutados-da685.firebaseapp.com',
-    databaseURL: 'https://enrutados-da685.firebaseio.com',
-    projectId: 'enrutados-da685',
-    storageBucket: 'enrutados-da685.appspot.com',
-    messagingSenderId: '50191944291',
-    appId: "1:50191944291:web:85693ee8aa28735369c656",
-    measurementId: "G-TT84JFJXC9"
+  apiKey: 'AIzaSyCjGsgPgUBt6lk3jxRAuYbyG4DGVxcdesY',
+  authDomain: 'enrutados-da685.firebaseapp.com',
+  databaseURL: 'https://enrutados-da685.firebaseio.com',
+  projectId: 'enrutados-da685',
+  storageBucket: 'enrutados-da685.appspot.com',
+  messagingSenderId: '50191944291',
+  appId: '1:50191944291:web:85693ee8aa28735369c656',
+  measurementId: 'G-TT84JFJXC9',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
+// Agrega la configuración de Firebase Auth
+const auth = getAuth();
 
 /*import { myFunction } from './lib/index.js';
 myFunction();*/
 
-import home from './home.js';
+import { home } from './home.js';
 import login from './login.js';
 import error from './error.js';
 
@@ -44,7 +47,6 @@ const root = document.getElementById('root');
 
 function navigateTo(hash) {
   const route = routes.find((routeFound) => routeFound.path === hash);
-  
   if (route && route.component) {
     window.history.pushState(
       {},
@@ -56,7 +58,7 @@ function navigateTo(hash) {
       root.removeChild(root.firstChild);
     }
     root.appendChild(route.component(navigateTo));
-   } else {
+  } else {
     navigateTo('/error');
   }
 }
